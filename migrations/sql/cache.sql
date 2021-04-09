ad05a76d2f02a6a4b12faed80e51b2432998bcd6
charescape <charescape@outlook.com>
2021-04-09 13:30:16 +0800

    DEFAULT NULL -> NULL

--- migrations/sql/cache.sql
+++ migrations/sql/cache.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS `dbcache`;
 CREATE TABLE `dbcache`
 (
   `id` varchar(128) NOT NULL,
-  `expire` int DEFAULT NULL,
+  `expire` int NULL,
   `data` mediumblob,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=`utf8mb4` COLLATE=`utf8mb4_unicode_ci`;
