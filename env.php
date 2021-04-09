388ec13c0efe0230b8f990051d1e0ccd5d9c51e1
charescape <charescape@outlook.com>
2021-04-09 09:59:27 +0800

    update

--- env.php
+++ env.php
@@ -1,7 +1,1 @@
 <?php
-
-/*
- * for framework constants, use `YII_` as a prefix
- * for custom constants, use `CC_` as a prefix
- * for wechat constants, use `WX_` as a prefix
- */
--- requirements.php
+++ requirements.php
@@ -17,8 +17,8 @@
 
 if (!isset($frameworkPath)) {
     $searchPaths = array(
-        dirname(__FILE__) . '/vendor/yiisoft/yii2',
-        dirname(__FILE__) . '/../vendor/yiisoft/yii2',
+        __DIR__ . '/vendor/ziiframework/zii/src',
+        __DIR__ . '/../vendor/ziiframework/zii/src',
     );
     foreach ($searchPaths as $path) {
         if (is_dir($path)) {
