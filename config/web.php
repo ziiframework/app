f12b2c3e547d78f65d14a2af1cc3da3e199d4e0a
charescape <charescape@outlook.com>
2022-01-03 13:48:03 +0800

    Update web.php

--- config/web.php
+++ config/web.php
@@ -1,5 +1,7 @@
 <?php declare(strict_types=1);
 
+use Symfony\Component\Yaml\Yaml;
+
 $ztmp_components = $ztmp_components ?? (require __DIR__ . '/components.php');
 
 $ztmp_enable_strict_parsing = true;
@@ -20,29 +22,57 @@ $ztmp_config = [
             ),
         ]),
     ],
-    'on beforeRequest' => static function (): void {
+    'on beforeRequest' => static function (yii\base\Event $event): void {
+        /** @var yii\web\Application $sender */
+        $sender = $event->sender;
+
+    },
+    'on afterRequest' => static function (yii\base\Event $event): void {
+        /** @var yii\web\Application $sender */
+        $sender = $event->sender;
+
+        $missing = $sender->getMissingTranslations();
+        if (!YII_ENV_PROD && !empty($missing)) {
+            foreach ($missing as $category => $texts) {
+                $po_file = ZDIR_ROOT . '/' . str_replace('_', '/', $category) . '.yml';
+
+                $po = Yaml::parseFile($po_file);
+                foreach (array_unique($texts) as $text) {
+                    $po[$text][ZII_LANG_ALIAS[ZII_LANG_CURRENT]] = '-';
+                }
+
+                $result = [];
+                foreach ($po as $k => $v) {
+                    foreach ($v as $kk => $vv) {
+                        $result["T_ _T $k"][$kk] = "T_ _T $vv";
+                    }
+                }
+
+                $dump = Yaml::dump($result, 2, 2);
+                $dump = str_replace("\n'T_ _T ", "\n\n'T_ _T ", $dump);
+                file_put_contents($po_file, str_replace('T_ _T ', '', $dump));
+            }
+        }
     },
     'as hostControl' => [
         'class' => yii\filters\HostControl::class,
+        // see https://stackoverflow.com/questions/2814002/private-ip-address-identifier-in-regular-expression
         'allowedHosts' => YII_ENV_PROD ? [
+            'xxx.com',
             '*.xxx.com',
-        ] : [
-            '*.xxx.test',
-        ],
+        ] : array_merge(
+            [
+                'xxx.cc',
+                '*.xxx.cc',
+
+                '127.*',
+                '10.*',
+                '192.168.*',
+            ],
+            array_map(fn (int $p2): string => "172.$p2.*", range(16, 31)),
+        ),
     ],
 ];
-
-if (YII_ENV_DEV) {
-    $ztmp_config['bootstrap'][] = 'debug';
-    $ztmp_config['modules']['debug'] = [
-        'class' => yii\debug\Module::class,
-        'panels' => [
-            'queue' => yii\queue\debug\Panel::class,
-        ],
-        'controllerNamespace' => 'yii\debug\controllers',
-        'allowedIPs' => ['*'],
-    ];
-}
 
 unset($ztmp_config['class']);
 
