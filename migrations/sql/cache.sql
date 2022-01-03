02c3ea41dd76b379adc87f2e43ff40601db23417
charescape <charescape@outlook.com>
2022-01-03 14:44:59 +0800

    remove useless files

--- migrations/sql/cache.sql
+++
@@ -1,9 +0,0 @@
-
-DROP TABLE IF EXISTS `dbcache`;
-CREATE TABLE `dbcache`
-(
-    `id`     varchar(128) NOT NULL,
-    `expire` int          NULL,
-    `data`   mediumblob,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=`utf8mb4` COLLATE=`utf8mb4_unicode_ci`;
--- migrations/sql/session.sql
+++
@@ -1,9 +0,0 @@
-
-DROP TABLE IF EXISTS `dbsession`;
-CREATE TABLE `dbsession`
-(
-    `id`     varchar(128) NOT NULL,
-    `expire` int          NULL,
-    `data`   mediumblob,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=`utf8mb4` COLLATE=`utf8mb4_unicode_ci`;
