f6ce2478594c0629ca291121e9ebcdaec42c0189
charescape <charescape@outlook.com>
2022-01-03 14:42:18 +0800

    sql

--- src/Migration/Sql/cache.sql
+++ src/Migration/Sql/cache.sql
@@ -2,8 +2,8 @@
 DROP TABLE IF EXISTS `dbcache`;
 CREATE TABLE `dbcache`
 (
-  `id` varchar(128) NOT NULL,
-  `expire` int DEFAULT NULL,
-  `data` mediumblob,
-  PRIMARY KEY (`id`)
+    `id`     varchar(128) NOT NULL,
+    `expire` int          NULL,
+    `data`   mediumblob,
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=`utf8mb4` COLLATE=`utf8mb4_unicode_ci`;
--- src/Migration/Sql/session.sql
+++ src/Migration/Sql/session.sql
@@ -2,8 +2,8 @@
 DROP TABLE IF EXISTS `dbsession`;
 CREATE TABLE `dbsession`
 (
-  `id` varchar(128) NOT NULL,
-  `expire` int DEFAULT NULL,
-  `data` mediumblob,
-  PRIMARY KEY (`id`)
+    `id`     varchar(128) NOT NULL,
+    `expire` int          NULL,
+    `data`   mediumblob,
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=`utf8mb4` COLLATE=`utf8mb4_unicode_ci`;
